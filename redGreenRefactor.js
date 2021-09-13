5de200fd36b2df548e1561c6f86c1c1c41d82037
Justin W Soto <soto@pdx.edu>
2021-09-13 13:55:00 -0700

    just the begining here, wrote out the first part for all my tests

---
+++ redGreenRefactor.js
@@ -0,0 +1,16 @@
+// create a function that returns the name property of an object.
+
+const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+
+getName(spot) //returns spot
+getName(character) // returns Aang
+
+// create a function that returns a new array with all the items in the original array and a new item pushed to the end.
+copyAndPush();
+
+// create a function that takes an array of strings capitalize all strings and filter out any string that starts with the letter F/f.
+capitalizeAndFilter();
+
+// Use the Futurama Quote API http://futuramaapi.herokuapp.com to return a single quote with the format:
+fetchQuotes();
---
+++ redGreenRefactor.test.js
@@ -0,0 +1,31 @@
+import { redGreenRefactor } from './redGreenRefactor.js';
+
+// test for getName function
+describe('getsName', () => {
+    it('should return the name property of an object', () => {
+        expect();
+    })
+})
+
+// test for copyAndPush function
+describe('copyAndPushes', () => {
+    it('should return a new array with all the items in the original array and a new item pushed to the end.', () => {
+        expect();
+    })
+})
+
+// test for capitalizeAndFilter function
+describe('capitalizesAndFilters', () => {
+    it('should take an array of strings capitalize all strings and filter out any string that starts with the letter F/f.', () => {
+        expect();
+    })
+})
+
+// test for fetchQUotes function
+describe('fetchesQuotes', () => {
+    it('should ', () => {
+        expect();
+    })
+})
+
+// test for mocks function
