114dc2a94c03e9140f3cbb73be16254fd125e5c0
Justin W Soto <soto@pdx.edu>
2021-09-13 15:00:12 -0700

    getName tests are passing

--- redGreenRefactor.js
+++ redGreenRefactor.js
@@ -3,14 +3,19 @@
 const spot = { name: 'spot', age: 5, weight: '20 lbs' };
 const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
 
-getName(spot) //returns spot
-getName(character) // returns Aang
+getName(spot); //returns spot
+getName(character); // returns Aang
+
+export function getName({ name }) {
+  return name;
+}
+
 
 // create a function that returns a new array with all the items in the original array and a new item pushed to the end.
-copyAndPush();
+// copyAndPush();
 
 // create a function that takes an array of strings capitalize all strings and filter out any string that starts with the letter F/f.
-capitalizeAndFilter();
+// capitalizeAndFilter();
 
 // Use the Futurama Quote API http://futuramaapi.herokuapp.com to return a single quote with the format:
-fetchQuotes();
+// fetchQuotes();
--- redGreenRefactor.test.js
+++ redGreenRefactor.test.js
@@ -1,31 +1,42 @@
-import { redGreenRefactor } from './redGreenRefactor.js';
+import { getName } from './redGreenRefactor.js';
 
 // test for getName function
-describe('getsName', () => {
-    it('should return the name property of an object', () => {
-        expect();
-    })
-})
+describe('getName', () => {
+  it('should return the name property of the spot object', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const expectedName = 'spot';
+    const actualName = getName(spot);
+    expect(actualName).toEqual(expectedName);
+    
+  });
 
-// test for copyAndPush function
-describe('copyAndPushes', () => {
-    it('should return a new array with all the items in the original array and a new item pushed to the end.', () => {
-        expect();
-    })
-})
+  it('should return the name property of the character object', () => {
+    const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    const expectedName = 'Aang';
+    const actualName = getName(character);
+    expect(actualName).toEqual(expectedName);
+  
+  });
 
-// test for capitalizeAndFilter function
-describe('capitalizesAndFilters', () => {
-    it('should take an array of strings capitalize all strings and filter out any string that starts with the letter F/f.', () => {
-        expect();
-    })
-})
+  // test for copyAndPush function
+  // describe('copyAndPush', () => {
+  //   it('should return a new array with all the items in the original array and a new item pushed to the end.', () => {
+  //     expect();
+  //   });
+  // });
 
-// test for fetchQUotes function
-describe('fetchesQuotes', () => {
-    it('should ', () => {
-        expect();
-    })
-})
+  // test for capitalizeAndFilter function
+  // describe('capitalizeAndFilter', () => {
+  //   it('should take an array of strings capitalize all strings and filter out any string that starts with the letter F/f.', () => {
+  //     expect();
+  //   });
+  // });
 
-// test for mocks function
+  // test for fetchQUotes function
+  // describe('fetchQuotes', () => {
+  //   it('should ', () => {
+  //     expect();
+  //   });
+  // });
+
+});
